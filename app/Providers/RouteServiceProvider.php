53677b88cfa57e3552847dac28fda919e9a580aa
sabreen mohammed <sabreen@wedomore.com>
2024-02-20 17:10:41 +0200

    edit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     protected $namespace = 'App\Http\Controllers\web';
-    protected $namespaceAdmin = 'App\Http\Controllers\admin';
+    protected $namespaceAdmin = '';
 
     /**
      * Define your route model bindings, pattern filters, etc.
